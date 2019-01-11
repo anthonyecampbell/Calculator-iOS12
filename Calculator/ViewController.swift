31e9a488f20857f5512927865b4422dc038fd90b
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2019-01-11 09:53:48 -0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Anthony Campbell on 01/11/2019.
+//  Copyright © 2019 Anthony Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
+
+}
+
