fa10572707729380a3dc962b3f6e9a831fdf2eb7
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2019-01-11 12:20:00 -0600

    Add functionality for numbers to appear in calculator screen Add functionality for clear, convert to opposite sign, convert to percent

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,19 +12,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert displayLabel.text to a Double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a non-number button is pressed
-    
+        // What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue *= -1
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayValue *= 0.01
+            }
+        }
+        
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
+        // What should happen when a number is entered into the keypad
     
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
+                displayLabel.text! += numValue
+            }
+        }
     }
 
 }
